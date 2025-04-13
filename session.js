a2e9af38ea02bfa7f033c3fc97edc8cc5dd533a5
hhdinuka <hhdinuka99@gmail.com>
2025-04-13 21:49:34 +0530

    Add files via upload

--- session.js
+++ session.js
@@ -1,4 +1,4 @@
-//══════════════════════════════════════════════════════════════════════════════════════════════════════//
+﻿//══════════════════════════════════════════════════════════════════════════════════════════════════════//
 //                                                                                                      //
 //                                   𝗤𝗨𝗘𝗘𝗡-𝗔𝗡𝗝𝗨 𝗫ᴾᴿᴼ 𝐁𝐎𝐓                                               //
 //                                                                                                      //
@@ -40,12 +40,12 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~jRt0XDbJ#wqxeA0i0OO1qHvP7FKl2Se2ZNTFOOXxG7ZPTW70N1QI",
-    GITHUB_TOKEN: process.env.GITHUB_TOKEN || "p1QPhXIK0sU16km1x8YOuxDw2XWokw3ua9u7",
-    GITHUB_USERNAME: process.env.GITHUB_USERNAME || "hhdinuka",
+    SESSION_ID: process.env.SESSION_ID || "your session id",
+    GITHUB_TOKEN: process.env.GITHUB_TOKEN || "github token without ghp_",
+    GITHUB_USERNAME: process.env.GITHUB_USERNAME || "github username",
     SUDO: process.env.SUDO ? process.env.SUDO.split(",") : ["94717775628", "94758775628"],
     PORT: process.env.PORT || "8000",
     SESSION_DIR: process.env.SESSION_DIR || "session",
-    FOOTER: process.env.FOOTER || "> *D A R K S H A D O W X T E A M*" 
+    FOOTER: process.env.FOOTER || "> QUEEN ANJU XPRO\n> Developed by Mr. Rashmika\n> GitHub Repository: github.com/Mrrashmika/QUEEN_ANJU_xPro\n\ud83c\udf1f Premium Version | Advanced Features | Secure & Reliable \ud83c\udf1f\n\u00a9 2025 QUEEN ANJU XPRO. All rights reserved." 
   };
   
