aa8ceda7f7861a40f5428c92249d8c412ddc1913
hhdinuka <hhdinuka99@gmail.com>
2025-04-13 21:54:50 +0530

    Update session.js

--- session.js
+++ session.js
@@ -40,8 +40,8 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "your session id",
-    GITHUB_TOKEN: process.env.GITHUB_TOKEN || "github token without ghp_",
+    SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~jRt0XDbJ#wqxeA0i0OO1qHvP7FKl2Se2ZNTFOOXxG7ZPTW70N1QI",
+    GITHUB_TOKEN: process.env.GITHUB_TOKEN || "MVsJPU5Sqs5sgAcn9CJRamUfjHLO0F3nr8ef",
     GITHUB_USERNAME: process.env.GITHUB_USERNAME || "hhdinuka",
     SUDO: process.env.SUDO ? process.env.SUDO.split(",") : ["94717775628", "94758775628"],
     PORT: process.env.PORT || "8000",
